254c35dbf1c8e2d64a3c8ea4a535a324ddfca0b8
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-15 07:23:21 -0400

    adds verbfile

---
+++ verbfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var fields = require('./lib/fields');
+
+module.exports = function(app, base) {
+  app.extendWith('verb-readme-generator');
+
+  app.helper('flags', function() {
+    return toList(fields).join('\n');
+  });
+
+  app.task('default', ['readme']);
+};
+
+function toList(obj) {
+  return Object.keys(obj).map(function(key) {
+    var flag = '- `--' + key + '`';
+    var fn = obj[key];
+    if (fn.help) flag += ': ' + fn.help;
+    return flag;
+  });
+}
