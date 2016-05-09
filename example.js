dcf58c3159d76981532240b25cc97779054ffa93
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-09 07:26:54 -0400

    add base-store to examples

--- example.js
+++ example.js
@@ -2,10 +2,12 @@
 
 var cli = require('./');
 var argv = require('minimist')(process.argv.slice(2));
+var store = require('base-store');
 var data = require('base-data');
 var Base = require('base');
 var base = new Base({isApp: true});
 base._name = 'verb';
+base.use(store('base-cli-process-example'));
 base.use(data());
 base.use(cli());
 
